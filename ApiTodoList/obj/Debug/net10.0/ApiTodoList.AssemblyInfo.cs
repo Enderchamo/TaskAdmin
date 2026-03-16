6aaa8931b93d1bfdda5ce8b3a088c36ad1e84206
Vic <gandergoku@gmail.com>
2026-03-16 01:54:09 -0400

    feat: Elimine los residuos de Weather forescast

--- ApiTodoList/obj/Debug/net10.0/ApiTodoList.AssemblyInfo.cs
+++ ApiTodoList/obj/Debug/net10.0/ApiTodoList.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiTodoList")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36180f9c1f529b8f878f2f2d1c047f0a314209fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed1d80ee8acddd5e1b4c3ca0ffaf1bacc5c87d0d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiTodoList")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiTodoList")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
