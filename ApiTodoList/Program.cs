0055cd12b9341e5f2016af3ede2d3d34605553cd
Vic <gandergoku@gmail.com>
2026-03-16 01:59:20 -0400

    feat: Registro de mi Servicio y Repositorio en program.

--- ApiTodoList/Program.cs
+++ ApiTodoList/Program.cs
@@ -1,6 +1,13 @@
+using ApiTodoList.Persistence;
+using ApiTodoList.Interfaces;
+using ApiTodoList.Service;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<ITaskRepository, JsonTaskRepository>();
+builder.Services.AddScoped<ITaskService, TaskService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
